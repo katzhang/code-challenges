007d8e89a5f2db01cf61cb284db345bb80306312
katzhang-ias <arcadychang@gmail.com>
2015-10-03 22:47:14 -0400

    met time limit for arithmetic progression

--- arithmetic-progressions/ariprog.cpp
+++ arithmetic-progressions/ariprog.cpp
@@ -5,97 +5,72 @@ LANG: C++
 */
 #include <iostream>
 #include <fstream>
-#include <ctime>
 #include <vector>
 #include <algorithm>
 
+
+#define MAX_M 250
+#define MAX_N 25
+
 using namespace std;
 
-bool contains(const vector<int>& s, int n) {
-    for (int i = 0; i < s.size(); ++i) {
-    	if (s[i] == n) {
-    		return true;
-    	}
-    	if (s[i] > n) {
-    		return false;
-    	}
-    };
-    return false;
-}
-
-vector<int> computeProg(int a, int b, int n) {
-	vector<int> prog;
-	for (int i = 0; i < n; ++i) {
-		prog.push_back(a + i * b);
-	}
-	return prog;
-}
+struct prog {
+    int a;
+    int b;
+    bool operator < (const prog& other) const{
+        return b < other.b;
+    }
+};
 
 int main() {
     ofstream fout ("ariprog.out");
     ifstream fin ("ariprog.in");
-    clock_t begin = clock();
 
     int N, M;
     fin >> N >> M;
-    int MAX_BISQUARES = M * M + M * M;
-    vector<int> bisquares;
-    vector<int> bisquare_diffs;
-    vector<int> result;
+    int MAX_BISQUARE = M * M * 2;
+    vector<prog> progs;
+
+    bool isBisquare[MAX_M * MAX_M * 2 + 10] = {false};
 
     for (int i = 0; i <= M; ++i) {
-    	for (int j = i + 1; j <= M; ++j) {
-    		bisquares.push_back(i * i + j * j);
+    	for (int j = 0; j <= M; ++j) {
+    		int bisquare = i * i + j * j;
+    		isBisquare[bisquare] = true;
     	}
-    	bisquares.push_back(i * i + i * i);
     };
 
-    sort(bisquares.begin(), bisquares.end());
 
-    for (int i = 0; i < bisquares.size(); ++i) {
-    	for (int j = i + 1; j < bisquares.size(); ++j) {
-    		int diff = bisquares[j] - bisquares[i];
-    		if (!contains(bisquare_diffs, diff)) {
-    			bisquare_diffs.push_back(diff);
+    for (int a = 0; a < MAX_BISQUARE; ++a) {
+    	if (isBisquare[a]) {
+    		for (int b = 1; b < (MAX_BISQUARE - a)/ N + 2 * N; ++b) {
+	    		bool isValid = true;
+	    		for (int n = 0; n < N; ++n) {
+	    			int prog = a + n * b;
+	    			if (!isBisquare[prog]) {
+	    				isValid = false;
+	    				break;
+	    			}
+	    		}
+	    		if (isValid) {
+	    			prog p;
+	    			p.a = a;
+	    			p.b = b;
+	    			progs.push_back(p);
+	    		}
     		}
     	}
     }
 
-    sort(bisquare_diffs.begin(), bisquare_diffs.end());
-    bisquare_diffs.erase(unique(bisquare_diffs.begin(), bisquare_diffs.end()), bisquare_diffs.end());
-
-    int everValid = false;
-
-    for (int j = 0; j < bisquare_diffs.size(); ++j) {
-    	int b = bisquare_diffs[j];
-    	if (b == 0) continue;
-
-    	for (int i = 0; i < bisquares.size(); ++i) {
-    		int a = bisquares[i];
-    		bool valid = true;
-    		vector<int> prog = computeProg(a, b, N);
-
-    		for (int m = 0; m < prog.size(); ++m) {
-    			if (!contains(bisquares, prog[m])) {
-    				valid = false;
-    				break;
-    			}
-    		}
-    		if (valid) {
-    			if (!everValid) {
-    				everValid = true;
-    			}
-    			fout << a << " " << b << endl;
-    		}
-    	}
-    }
-    if (!everValid) {
+    if(progs.size() != 0){
+        sort(progs.begin(), progs.end());
+        for(int i = 0; i < progs.size(); ++i){
+            fout << progs[i].a << " " << progs[i].b << endl;
+        }
+    } else {
     	fout << "NONE" << endl;
     }
 
-    clock_t end = clock();
-    double elapsed = double(end - begin);
-    cout << "time elapsed: " << elapsed << endl;
 
 }
 
